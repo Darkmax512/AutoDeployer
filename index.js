4fca2aba6f3650c63b7f9d68080e71547d0f4718
Sayed Helmy <s.helmy50@gmail.com>
2022-11-02 07:11:15 +0200

    main change

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ const { exec } = require("child_process");
 const express = require("express");
 const app = express();
 
-app.post("/backend", (_, res) => {
-  exec(`cd /root/egycards-back && git pull && pm2 restart egy`);
+app.use(express.json());
+
+app.post("/backend", (req, res) => {
+  const { ref } = req.body;
+  if (ref !== "refs/heads/main") return res.sendStatus(400);
+  exec(
+    `cd /root/webapp && git stash && git pull && npm i && npm run build && pm2 restart Tianzu`
+  );
   res.sendStatus(200);
 });
 
-app.post("/frontend", (_, res) => {
-  exec(`cd /root/Gift-Cards-App && git pull`);
+app.post("/frontend", (req, res) => {
+  const { ref } = req.body;
+  if (ref !== "refs/heads/main") return res.sendStatus(400);
+  exec(
+    `cd /root/webapp-fe && git stash && git pull && npm i --legacy-peer-deps > /var/log/frontend-install.log && npm run build > /var/log/frontend-build.log && pm2 restart Tianzu`
+  );
   res.sendStatus(200);
 });
 
-const port = process.env.PORT || 5074;
+const port = 5074;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
